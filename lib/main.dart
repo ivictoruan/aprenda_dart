9d4aae2c9f14ee033260997bcc7ca20b24cc85ab
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-08-04 15:31:27 -0300

    REFACTOR: Colocando transparencia na parte de cima do appbar;

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:aprenda_dart/src/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
   runApp(const AppWidget());
 }
