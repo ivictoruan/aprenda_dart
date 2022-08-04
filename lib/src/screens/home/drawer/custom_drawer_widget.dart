40ead840bfaf1722402a6c9746753565a718538d
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-08-04 15:33:09 -0300

    CHORE: add transparencia;

--- lib/src/screens/home/drawer/custom_drawer_widget.dart
+++ lib/src/screens/home/drawer/custom_drawer_widget.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:aprenda_dart/src/shared/constants.dart';
 import 'package:flutter/material.dart';
 
@@ -10,63 +8,71 @@ class CustomDrawerWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Drawer(
-        backgroundColor: Colors.black,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Stack(
-              children: [
-                Image.network(
-                  'https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80',
-                  // height: 200,
-                  fit: BoxFit.fill,
+      backgroundColor: Colors.transparent.withOpacity(0.81),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              // BackdropFilter(filter: filter)
+              // Image.network(
+              //     'https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80',
+              //     // height: 200,
+              //     fit: BoxFit.fill,
+              //     color: const Color.fromRGBO(255, 255, 255, 0.7),
+              //     colorBlendMode: BlendMode.modulate),
+              Padding(
+                padding: EdgeInsets.only( top: size.height * 0.05),
+                child: Image.network(
+                  'https://seeklogo.com/images/D/dart-logo-FDA1939EC4-seeklogo.com.png',
+                  width: size.width * 0.22,
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 18, top: 60),
-                  child: Image.network(
-                    'https://seeklogo.com/images/D/dart-logo-FDA1939EC4-seeklogo.com.png',
-                    width: size.width * 0.22,
+              ),
+              
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top:18.0),
+            child: Text(
+                  'Aprenda Dart',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: size.width * 0.05,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
-                const Padding(
-                  padding: EdgeInsets.only(left: 18, top: 140),
-                  child: Text(
-                    'Aprenda Dart',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
+          ),
+          SafeArea(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: drawerContent.length,
+              itemBuilder: (context, index) => InkWell(
+                onTap: () {}, // acessar link externo
+                child: ListTile(
+                  textColor: Colors.white,
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.transparent,
+                    radius: size.width * 0.04,
+                    child: Icon(
+                      drawerContent[index]['icon'] as IconData,
+                      size: size.width * 0.085,
+                      // color: Colors.white,
                     ),
                   ),
-                ),
-              ],
-            ),
-            SafeArea(
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: drawerContent.length,
-                itemBuilder: (context, index) => InkWell(
-                  onTap: () {},
-                  child: const Padding(
-                    padding:  EdgeInsets.all(8.0),
-                    child: ListTile(
-                      // tileColor: Colors.
-                      textColor: Colors.white,
-                      
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.transparent,
-                        // foregroundColor: Colors.white,
-                        child: Icon(Icons.eco_rounded),
-                      ),
-                      title:  Text('Conteúdos sobre Dart'),
+                  title: Text(
+                    drawerContent[index]['titleContent'] as String,
+                    style: TextStyle(
+                      fontSize: size.width * 0.048,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: 0.8,
                     ),
                   ),
                 ),
               ),
             ),
-            // ListView(),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
   }
 }
